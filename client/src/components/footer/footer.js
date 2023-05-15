80ff9abbf048db5c1df6d309b239586f9ababea1
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2023-05-15 18:20:08 +0530

    NavLink remove footer.js

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
 
 import { FaDiscord, FaInstagram, FaYoutube } from "react-icons/fa";
 
